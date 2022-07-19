a3bb687ccca9f3ab040f4c5e792040c30555fb5f
RyannKim327 <rksesgundo123@gmail.com>
2022-07-19 11:10:44 +0800

    07-19-22 11:10

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const sha = require("js-sha256")
 const app = express()
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
 	
